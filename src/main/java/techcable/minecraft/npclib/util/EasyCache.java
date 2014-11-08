6d311111109424f9a1947a00c3c16729841d6a85
Techcable <Techcable@outlook.com>
2014-11-08 18:31:38 +0000

    Add Easy Cache

---
+++ src/main/java/techcable/minecraft/npclib/util/EasyCache.java
@@ -0,0 +1,34 @@
+package techcable.minecraft.npclib.util;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+
+import lombok.*;
+
+@Getter
+public class EasyCache<K, V> {
+    private final Cache<K, V> backing;
+    public EasyCache(Loader<K, V> loader) {
+        backing = CacheBuilder.newBuilder().weakKeys().weakValues().build(new LoaderCacheLoader(loader));
+    }
+    
+    public V get(K key) {
+        return backing.getUnchecked(key);
+    }
+    
+    public static interface Loader<K, V> {
+        public V load(K key);
+    }
+    
+    @RequiredArgsConstructor
+    @Getter
+    private static class LoaderCacheLoader<K, V> implements CacheLoader {
+        private final Loader<K, V> backing;
+        
+        @Override
+        public V load(K key) {
+            return backing.load(key);
+        }
+    }
+}
