6f6290cc8fafd2614f99368aab6598e2d19aecf7
MrLittleKitty <nuclearcat1337@gmail.com>
2015-03-30 18:00:08 -0700

    Updated some stuff
    Moved some stuff around and fixed some methods.

--- nms-v1_8_R2/src/main/java/net/techcable/npclib/nms/versions/v1_8_R2/NMS.java
+++ nms-v1_8_R2/src/main/java/net/techcable/npclib/nms/versions/v1_8_R2/NMS.java
@@ -201,8 +201,8 @@ public class NMS implements net.techcable.npclib.nms.NMS {
 
 	@Override
 	public void onDespawn(NPC npc) {
-  	    WorldServer world = getHandle(npc.getLocation().getWorld());
-            world.removeEntity(getHandle(npc.getEntity()));
             sendPacketsTo(Bukkit.getOnlinePlayers(), new PacketPlayOutPlayerInfo(EnumPlayerInfoAction.REMOVE_PLAYER, getHandle((Player)npc.getEntity())));
+	    WorldServer world = getHandle(npc.getEntity().getLocation().getWorld());
+            world.removeEntity(getHandle(npc.getEntity()));
 	}
 }
