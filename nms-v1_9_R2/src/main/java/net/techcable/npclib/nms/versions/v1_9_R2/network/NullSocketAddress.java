938fdbf7df8d2ba8c65b0067bafd2a72aebfffbc
Rigner <frasse_l@epitech.eu>
2016-08-25 01:31:55 +0200

    Done 1.9.4 (not tested)

---
+++ nms-v1_9_R2/src/main/java/net/techcable/npclib/nms/versions/v1_9_R2/network/NullSocketAddress.java
@@ -0,0 +1,9 @@
+package net.techcable.npclib.nms.versions.v1_9_R2.network;
+
+import java.net.SocketAddress;
+
+public class NullSocketAddress extends SocketAddress {
+
+    private static final long serialVersionUID = 1L;
+
+}
